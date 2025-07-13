e8f5f302b215102b0afd57d37a3e1adfb6fcc3ac
Leandro Ferreira <leandro.fcode@gmail.com>
2025-07-13 17:30:25 -0300

    update readme

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,12 @@
 import PageNotFound from "./pages/PageNotFound";
 import PageLayout from "./components/PageLayout";
 import Home from "./pages/Home";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import ProjectDocumentation from "./pages/ProjectDocumentation";
 
 const Router = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<PageLayout />}>
           <Route index element={<Home />}/>
@@ -14,7 +14,7 @@ const Router = () => {
           <Route path="*" element={<PageNotFound />}/>
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
  
