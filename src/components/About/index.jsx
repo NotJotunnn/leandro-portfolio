d515906549a39476a7bf9568f63942419066379e
Leandro Ferreira <leandro.fcode@gmail.com>
2025-07-15 11:19:25 -0300

    update study list

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -58,7 +58,7 @@ const About = () => {
               aria-label="Veja meu currículo"
               rel="canonical"
               target="_blank"
-              href="assets/Leandro-Ferreira-250714.pdf"
+              href="assets/Leandro-Ferreira-250715.pdf"
             >
               <Button>Baixar currículo</Button>
             </a>
