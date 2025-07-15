3fa942086824063b6b8dbace36f6c3734d2b2f04
Leandro Ferreira <leandro.fcode@gmail.com>
2025-07-15 11:18:16 -0300

    update study list

--- src/store/reducers/studies.js
+++ src/store/reducers/studies.js
@@ -3,54 +3,58 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = [
   {
     title: "CSSstudies",
-    url: "https://github.com/NotJotunnn/CSSstudies",
+    url: "/NotJotunnn/CSSstudies",
     description: "Some of my favorite little css studies",
+    star: true,
   },
   {
     title: "E-commerce-Project",
-    url: "https://github.com/NotJotunnn/E-commerce-Project",
+    url: "/NotJotunnn/E-commerce-Project",
     description:
       "Real life study of a company and development of an E-commerce",
-  },
-  {
-    title: "Object-Oriented-Studies",
-    url: "https://github.com/NotJotunnn/Object-Oriented-Studies",
-    description: "N/A",
-  },
-  {
-    title: "Projeto-CRUD",
-    url: "https://github.com/NotJotunnn/Projeto-CRUD",
-    description: "Projeto da aula de Web quinta",
-  },
-  {
-    title: "netlify-study",
-    url: "https://github.com/NotJotunnn/netlify-study",
-    description: "N/A",
-  },
-  {
-    title: "Workflow-projects",
-    url: "https://github.com/NotJotunnn/Workflow-projects",
-    description: "Some of the projects I've used to better my workflow!",
+    star: true,
   },
   {
     title: "Projects",
-    url: "https://github.com/NotJotunnn/Projects",
+    url: "/NotJotunnn/Projects",
     description: "A few of my projects from past year",
+    star: true,
+  },
+  {
+    title: "Alura-studies",
+    url: "/NotJotunnn/Alura-studies",
+    description: "Todos os estudos que fiz durante meus estudos",
+    star: false,
+  },
+  {
+    title: "Object-Oriented-Studies",
+    url: "/NotJotunnn/Object-Oriented-Studies",
+    description: "N/A",
+    star: false,
+  },
+  {
+    title: "Projeto-CRUD",
+    url: "/NotJotunnn/Projeto-CRUD",
+    description: "Projeto da aula de Web quinta",
+    star: false,
+  },
+  {
+    title: "Workflow-projects",
+    url: "/NotJotunnn/Workflow-projects",
+    description: "Some of the projects I've used to better my workflow!",
+    star: false,
   },
   {
     title: "Rocket-Projects",
-    url: "https://github.com/NotJotunnn/Rocket-Projects",
+    url: "/NotJotunnn/Rocket-Projects",
     description: "A collection of past projects from Rocketseat done(or not)",
+    star: false,
   },
   {
     title: "Scrimba-Projects",
-    url: "https://github.com/NotJotunnn/Scrimba-Projects",
+    url: "/NotJotunnn/Scrimba-Projects",
     description: "Some of my projects from the Scrimba course!",
-  },
-  {
-    title: "churrascoProject",
-    url: "https://github.com/NotJotunnn/churrascoProject",
-    description: "N/A",
+    star: false,
   },
 ];
 
