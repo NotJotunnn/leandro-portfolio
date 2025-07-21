ce1fe722e3840ad22563151308175e0589847e46
Leandro Ferreira <leandro.fcode@gmail.com>
2025-07-21 15:53:53 -0300

    fix project not displaying

--- src/store/Projects/index.js
+++ src/store/Projects/index.js
@@ -1,7 +1,8 @@
 import ChurrascoFerreira from "./ChurrascoFerreira";
 import LokinhoSkins from "./LokinhoSkins";
 import Portfolio from "./Portfolio";
+import Ecommerce from "./Ecommerce";
 
-const projects = [LokinhoSkins, ChurrascoFerreira, Portfolio];
+const projects = [LokinhoSkins, ChurrascoFerreira, Portfolio, Ecommerce];
 
 export default projects;
